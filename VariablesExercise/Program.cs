ea302c306787d1ecf5f1606b4b0d983bf8760dd7
tishacapps0519 <cappstisha@gmail.com>
2025-04-21 20:10:31 -0400

    Variables-Exercise Assignment

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Latitia";
+            int age = 43;
+            double avg = 98.7;
+            decimal bankAccount = 10000.00m;
+            char initial = 'M';
+            bool isMarried = true;
+            
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial {initial}\nMarried: {isMarried} ");
         }
     }
 }
